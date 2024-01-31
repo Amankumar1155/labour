7fa4cb1c2e39e9227e07bdae5ab996f18a84a5a2
Aman Chaursiya <41237180+Amankumar1155@users.noreply.github.com>
2024-02-01 01:35:28 +0530

    new

---
+++ app/src/main/java/com/aman/labour/partnerregistration.java
@@ -0,0 +1,14 @@
+package com.aman.labour;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class partnerregistration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_partnerregistration);
+    }
+}
