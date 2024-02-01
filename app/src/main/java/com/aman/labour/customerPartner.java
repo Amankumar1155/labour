f2b323e427402aa7f60e1462b0a8eafb2de9d949
Aman Chaursiya <41237180+Amankumar1155@users.noreply.github.com>
2024-02-01 07:44:56 +0530

    newaaaaaa

--- app/src/main/java/com/aman/labour/customerPartner.java
+++ app/src/main/java/com/aman/labour/customerPartner.java
@@ -7,18 +7,29 @@ import android.view.View;
 import android.widget.Button;
 
 public class customerPartner extends AppCompatActivity {
-    Button btn;
+    Button btn1;
+    Button btn2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_partner);
-        btn = findViewById(R.id.part12);
-        btn.setOnClickListener(new View.OnClickListener() {
+        btn1 = findViewById(R.id.part12);
+        btn2=findViewById(R.id.customerregister);
+        btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(customerPartner.this, partnerregistration.class));
                 finish();
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(customerPartner.this, customerregisation.class));
+                finish();
+
             }
         });
     }
