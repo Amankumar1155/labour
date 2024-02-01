68536955f0078803757da21b74b8af27ec80da6c
Aman Chaursiya <41237180+Amankumar1155@users.noreply.github.com>
2024-02-01 05:48:54 +0530

    newaa

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,6 +32,9 @@ android {
 }
 
 dependencies {
+    implementation("com.google.android.material:material:1.2.1")
+    implementation("com.google.android.material:material:1.11.0")
+
 
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
