4d92d3e7b1a90d5481a390f44e381e550992d200
Aman Chaursiya <41237180+Amankumar1155@users.noreply.github.com>
2024-02-03 11:50:39 +0530

    newaaaaaaaaaaaa

--- app/src/main/java/com/aman/labour/splash.java
+++ app/src/main/java/com/aman/labour/splash.java
@@ -15,10 +15,11 @@ public class splash extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(splash.this, MainActivity.class);
+                Intent intent = new Intent(splash.this, dashboardhome.class);
                 startActivity(intent);
                 finish();
             }
         }, 2000);
+
     }
 }
