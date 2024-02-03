582f3f1654e9b51c0c050d2147b134351089d009
Aman Chaursiya <41237180+Amankumar1155@users.noreply.github.com>
2024-02-03 14:34:09 +0530

    newaaaaaaaaaaaassdsd

--- app/src/main/java/com/aman/labour/Dashboard.java
+++ app/src/main/java/com/aman/labour/Dashboard.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class dashboardhome extends AppCompatActivity {
+public class Dashboard extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboardhome);
+        setContentView(R.layout.activity_dashboard);
     }
 }
--- app/src/main/java/com/aman/labour/splash.java
+++ app/src/main/java/com/aman/labour/splash.java
@@ -15,7 +15,7 @@ public class splash extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(splash.this, dashboardhome.class);
+                Intent intent = new Intent(splash.this, Dashboard.class);
                 startActivity(intent);
                 finish();
             }
