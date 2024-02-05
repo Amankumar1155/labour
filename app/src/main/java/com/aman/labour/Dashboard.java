4b689cc3278903adf51322d9a093bbdd11479fad
Aman Chaursiya <41237180+Amankumar1155@users.noreply.github.com>
2024-02-05 16:16:10 +0530

    newaaaasDsfdfsds

--- app/src/main/java/com/aman/labour/Dashboard.java
+++ app/src/main/java/com/aman/labour/Dashboard.java
@@ -1,36 +1,92 @@
 package com.aman.labour;
 
 import androidx.annotation.NonNull;
+import android.Manifest;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.google.android.material.navigation.NavigationView;
 
-public class Dashboard extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class Dashboard extends AppCompatActivity {
+    Button refer,urllink;
+    Button btn;
     DrawerLayout drawerLayout;
 
     ActionBarDrawerToggle actionBarDrawerToggle;
     NavigationView navigationView;
 
+    @SuppressLint({"WrongViewCast", "MissingInflatedId"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
-        drawerLayout=findViewById(R.id.drawer_layout);
-        navigationView=findViewById(R.id.nav_view);
 
-        actionBarDrawerToggle =new ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close);
+
+        refer = findViewById(R.id.reffer);
+          urllink = findViewById(R.id.rateplay);
+
+
+        btn = findViewById(R.id.uplodebtn1);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Dashboard.this,ImageUplode.class));
+                finish();
+            }
+        });
+
+        refer.setOnClickListener(v -> {;
+            refer(Dashboard.this);
+        });
+        urllink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://alltradsolutions.com/";
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(intent);
+
+            }
+        });
+
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close);
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
         actionBarDrawerToggle.syncState();
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
 
+    private void refer(Dashboard dashboard) {
+        final String appPackageName = dashboard.getPackageName();
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_TEXT,"Download Now: https://alltradsolutions.com/" + appPackageName);
+        sendIntent.setType("text/plain");
+        dashboard.startActivity(sendIntent);
     }
 
     @Override
@@ -40,7 +96,6 @@ public class Dashboard extends AppCompatActivity {
         {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 }
