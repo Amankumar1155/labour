1fd5a9322147c3baea62a246d280fbf3d53c97ed
Aman Chaursiya <41237180+Amankumar1155@users.noreply.github.com>
2024-02-13 00:04:52 +0530

    newaaajasDsfdjfsdds

---
+++ app/src/main/java/com/aman/labour/Loginpage.java
@@ -0,0 +1,33 @@
+package com.aman.labour;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class Loginpage extends AppCompatActivity {
+
+    TextView btn;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loginpage);
+
+
+
+        btn=findViewById(R.id.passreset);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Loginpage.this,Forgottpage.class));
+                finish();
+            }
+        });
+
+    }
+}
--- app/src/main/java/com/aman/labour/MainActivity.java
+++ app/src/main/java/com/aman/labour/MainActivity.java
@@ -6,17 +6,39 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btn;
+    TextView btn1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        btn1=findViewById(R.id.Loginpage);
         btn=findViewById(R.id.btn_continue);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(MainActivity.this,Loginpage.class));
+                finish();
+
+
+            }
+        });
+
+
+
+
+
+
+
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v)
