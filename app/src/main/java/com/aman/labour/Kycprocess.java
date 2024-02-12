b9d676adc044c9eafd1efb260667424d6226e385
Aman Chaursiya <41237180+Amankumar1155@users.noreply.github.com>
2024-02-12 17:06:38 +0530

    newaaajasDsfdjfsdds

---
+++ app/src/main/java/com/aman/labour/Kycprocess.java
@@ -0,0 +1,29 @@
+package com.aman.labour;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Kycprocess extends AppCompatActivity {
+
+    CardView btn1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kycprocess);
+
+        btn1=findViewById(R.id.docsubmit);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Kycprocess.this, Dashboard.class));
+                finish();
+            }
+        });
+    }
+}
