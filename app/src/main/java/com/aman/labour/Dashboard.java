98b7cf8fcb47b3d0916760690a6598154f53a428
Aman Chaursiya <41237180+Amankumar1155@users.noreply.github.com>
2024-02-14 02:27:51 +0530

    newaaajasDsfdjfsdds

--- app/src/main/java/com/aman/labour/Dashboard.java
+++ app/src/main/java/com/aman/labour/Dashboard.java
@@ -2,6 +2,7 @@ package com.aman.labour;
 
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
+import android.content.ClipData;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -9,6 +10,7 @@ import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.Toast;
 
@@ -21,7 +23,7 @@ import com.google.android.material.navigation.NavigationView;
 
 public class Dashboard extends AppCompatActivity {
 
-
+ImageView imageView;
     DrawerLayout drawerLayout;
     ActionBarDrawerToggle actionBarDrawerToggle;
     NavigationView navigationView;
@@ -38,21 +40,46 @@ public class Dashboard extends AppCompatActivity {
 
 
 
-
-
-
         drawerLayout = findViewById(R.id.drawer_layout);
 
         NavigationView navigationViews = findViewById(R.id.nav_view);
         View headerView = navigationViews.getHeaderView(0);
 
         drawerLayout = findViewById(R.id.drawer_layout);
-        drawerToggle = new ActionBarDrawerToggle(
-                this,
-                drawerLayout,
-                R.string.navigation_drawer_open,
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.navigation_drawer_open,
                 R.string.navigation_drawer_close
         );
+        drawerLayout.addDrawerListener(drawerToggle);
+        drawerToggle.syncState();
+
+        navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+
+            if (itemId == R.id.nav_home)
+            {
+                Toast.makeText(this, "This is home", Toast.LENGTH_SHORT).show();
+            } else if (itemId==R.id.nav_profile)
+            {
+                Toast.makeText(this, "This is profile", Toast.LENGTH_SHORT).show();
+            }
+
+            drawerLayout.closeDrawer(navigationView);
+
+            return true;
+        });
+
+
+            imageView=findViewById(R.id.tv_drawer_nav);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!drawerLayout.isDrawerOpen(navigationView)) {
+                    drawerLayout.openDrawer(navigationView);
+                }
+            }
+        });
+
 
     }
 }
