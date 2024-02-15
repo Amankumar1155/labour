21abd54c53eacc4c7c82770bd0b702a8dff0218b
Aman Chaursiya <41237180+Amankumar1155@users.noreply.github.com>
2024-02-16 01:01:48 +0530

    newaaajfasDsfdjfsdds

--- app/src/main/java/com/aman/labour/MainActivity.java
+++ app/src/main/java/com/aman/labour/MainActivity.java
@@ -2,26 +2,45 @@ package com.aman.labour;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.net.Uri;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView call,whatsap;
 
 
     Button btn;
-    TextView btn1;
+    TextView btn1,btn2;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
 
+        call=findViewById(R.id.calldailer);
+        whatsap=findViewById(R.id.whatsaplaunch);
+        btn2=findViewById(R.id.websiteurl);
         btn1=findViewById(R.id.Loginpage);
         btn=findViewById(R.id.btn_continue);
+
+
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,Webview.class));
+                finish();
+            }
+        });
 
 
         btn1.setOnClickListener(new View.OnClickListener() {
@@ -35,10 +54,23 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        call.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("QueryPermissionsNeeded")
+            @Override
+            public void onClick(View v) {
+                String phoneNumber = "tel:7398869340";
 
 
+                Intent dialIntent = new Intent(Intent.ACTION_DIAL, Uri.parse(phoneNumber));
 
 
+                if (dialIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(dialIntent);
+                } else {
+                    Toast.makeText(MainActivity.this, "No dialer app found", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
 
         btn.setOnClickListener(new View.OnClickListener() {
